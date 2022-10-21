d8efac8986068227c15d08233a9af5bfcf8a0f7d
jingchao <finalpjc@gmail.com>
2022-10-21 11:37:33 +0800

    remove pricing link

--- src/components/Panels.vue
+++ src/components/Panels.vue
@@ -16,7 +16,7 @@
                   <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-down"></use>
                 </svg></a></li>
             <li><a href="#usecase">Use Cases</a></li>
-            <li><a href="https://docsend.com/view/4yjh7wivyxkg9s5z" target="_blank">Pricing</a></li>
+            <li><a href="#" target="_blank">Pricing</a></li>
             <li><a href="#blog">Blog</a></li>
             <li><a href="#about">About</a></li>
           </ul>
